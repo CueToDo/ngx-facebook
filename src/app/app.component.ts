d51b949571b0b357932922cba56440272c64cfe8
CueToDo <alex@Q2Do.com>
2017-06-04 21:16:14 +0100

    ngx-facebook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookService, InitParams, LoginOptions, LoginResponse, LoginStatus } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  loggedIn: boolean = false;
+  name: string = '';
+  email: string = '';
+  imgUrl: string = '';
+  accessToken: string = '';
+  showMore: boolean = false;
+
+  constructor(private fb: FacebookService) {
+
+    let initParams: InitParams = {
+      appId: '1114396292039346',
+      xfbml: true,
+      version: 'v2.8'
+    };
+
+    fb.init(initParams);
+  }
+
+
+  ngOnInit() {
+    this.getLoginStatus();
+  }
+
+  loginOptions: LoginOptions = {
+
+  };
+
+
+
+  // Facebook API call for getting user data
+  getFacebookData() {
+    this.fb.api('/me?fields=id,name,email,picture').then(
+      (response: any) => {
+        this.name = response.name;
+        this.email = response.email;
+        this.imgUrl = response.picture.data.url;
+      },
+      (error: any) => console.error(error)
+    );
+  }
+
+  login() {
+    this.fb.login(this.loginOptions).then(
+      (response: LoginResponse) => {
+        this.loggedIn = true;
+        this.accessToken = response.authResponse.accessToken;
+        this.getFacebookData();
+      },
+      (error: any) => console.error(error)
+    );
+  }
+
+  getLoginStatus() {
+    this.fb.getLoginStatus().then(
+      (response: LoginStatus) => {
+        if (response.status === 'connected') {
+          this.loggedIn = true;
+          this.accessToken = response.authResponse.accessToken;
+          this.getFacebookData();
+        }
+      },
+      (error: any) => console.error(error)
+    )
+  }
+
+  logout() {
+    this.fb.logout().then(
+      (response: LoginResponse) => {
+        this.loggedIn = false;
+        this.accessToken = '';
+      },
+      (error: any) => console.error(error)
+    );
+  }
 }
